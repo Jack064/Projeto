eddf55177124aacea5774d6c5524b2f7b5e9626d
Jack064 <jacsoncaetano2004@gmail.com>
2024-05-30 09:43:59 -0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,"index.html")
+
+def about(request):
+    return render(request,"about.html")
+
+def gallery(request):
+    return render(request,"gallery.html")
